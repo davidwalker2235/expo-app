f64c2169a1773096ace71dbd08aa34e1c7509800
davidwalker2235 <davidwalker2235@gmail.com>
2024-11-14 18:41:38 +0100

    add routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome World!</ThemedText>
+        <ThemedText type="title">Welcome all!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
